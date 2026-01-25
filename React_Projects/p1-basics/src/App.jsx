c169def03dd60184dd93e422bbbc12ade6706c4b
Pratham <pratham10600@gmail.com>
2026-01-25 16:45:46 +0530

    react-p1

---
+++ React_Projects/p1-basics/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>
+        Hi I am Pratyaksh working on React
+      </p>
+    </>
+
+    // it means it will get app.css first then index.css propeties
+  )
+}
+
+export default App
---
+++ React_Projects/precommands.md
@@ -0,0 +1,8 @@
+npm create vite@latest
+
+to run-> go to folder and run-> npm run dev
+How things are working?
+
+![alt text](image.png)
+
+to create component use rafce then enter using extension of react
