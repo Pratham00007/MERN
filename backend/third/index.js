a5d04768524a73092ff4df40b1f312276b913914
Pratham <pratham10600@gmail.com>
2026-01-29 18:50:05 +0530

    basic structure

---
+++ backend/third/index.js
@@ -0,0 +1,15 @@
+// including express module and intitalising an app
+const express =require  ('express');
+const app=express();
+
+//variable that stores the port number 
+const port=3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+// start your app or server     
+app.listen(port,()=>{
+    console.log("Application start ho chuki hai ");
+});
