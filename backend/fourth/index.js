7a87399ae6a05da8ac2b2805b0d63ab9c999647e
Pratham <pratham10600@gmail.com>
2026-01-29 19:30:41 +0530

    middleware

---
+++ backend/fourth/index.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const app=express()
+const port =3000    
+
+
+
+// loading middleware into the app
+app.use(express.json());
+app.get('/',(req,res)=>{
+    console.log(req.body);
+    res.send("Hello World")
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port  ${port}`)
+}) 
