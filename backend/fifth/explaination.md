1d5bc31711125642b729d54c790932fa774fce93
Pratham <pratham10600@gmail.com>
2026-02-04 18:56:16 +0530

    update n delete

--- backend/fifth/explaination.md
+++ backend/fifth/explaination.md
@@ -3,3 +3,10 @@ Before
 
 Coonection mongoose with mongodb
 ![alt text](image-1.png)
+
+on cloud worked
+![alt text](image-2.png)
+
+![alt text](image-3.png)
+
+![alt text](image-4.png)
--- backend/fifth/routes/Users.js
+++ backend/fifth/routes/Users.js
@@ -40,4 +40,55 @@ router.post('/users',async(req,res)=>{
     }
 })
 
+// update
+router.put('/users/:id',async(req,res)=>{
+    const {id} =req.params;
+    const {name,age,weight}=req.body;
+
+    try{
+        // purana data retturn
+        const updateUser =await User.findByIdAndUpdate(id,{name,age,weight});
+        // to check user must no tbe empty
+        if(!updateUser){
+            res.json({
+                message:"User not found"
+            })
+        }
+        // if succesfully updates
+        res.status(200).json({
+            success:true,
+            user:updateUser
+        })
+
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+})
+
+
+// delete
+router.delete('users/:id',async(req,res)=>{
+    const {id}=req.params;
+    try{const deletedUser=await User.findByIdAndDelete(id);
+        if(!deletedUser){
+            res.json({
+                message: "User not found"
+            })
+        }
+        // if user found and deleted
+        res.status(200).json({
+            success:true,
+            user:deletedUser
+        })
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+})
+
 module.exports=router;
